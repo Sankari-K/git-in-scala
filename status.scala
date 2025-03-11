642790ea0d6c149502b5fac243f5b9f33fc02b67
Sankari Karthik <sankarikarthik03#gmail.com>
2025-03-11 20:30:55 +0530

    Take care of edge case when checking for modified files (the file being checked could be deleted) and prevent creation of empty INDEX file if nothing is staged yet

--- status.scala
+++ status.scala
@@ -2,11 +2,19 @@ package gitcommands
 
 import datastructs.* 
 import fileops.*
-import java.nio.file.{Paths}
+import java.nio.file.{Paths, Files}
 import scala.collection.immutable.Stream.Cons
 
 def getStatus(currentDir: String): Unit = {
     val index = new Index(currentDir)
+
+    if (!index.isIndexInitialized()) {
+        println("on branch master\n")
+        println("no commits yet\n")
+        println("nothing to commit (create/copy files and use 'git add' to track)\n")
+        sys.exit(1)
+    }
+    
     index.initializeIndex()
 
     println(Console.WHITE + "changes to be committed: ")
@@ -32,8 +40,12 @@ def getStatus(currentDir: String): Unit = {
     println("\t(use \"add\" to update what will be committed)")
     println("\t(use \"restore\" to discard changes in working directory)")
     for ((file, (oldhash, newhash)) <- index.getIndex) {
-        if (newhash != computeFileHash(file)) {
-            println("\t" + Console.RED + "modified: " + file)
+        // newhash is checked with null to make sure file still exists
+        // if it doesn't exist, it would be a deleted file anyway
+        if (newhash != "null") {
+            if (newhash != computeFileHash(file)) {
+                println("\t" + Console.RED + "modified: " + file)
+            }
         }
     }
     println()
