fe2bb3d477d4cc076f95effb366f9582e3fba5a9
Sankari Karthik <sankarikarthik03#gmail.com>
2025-03-11 20:52:02 +0530

    Add more logging

--- remove.scala
+++ remove.scala
@@ -13,10 +13,14 @@ def removeFile(currentDir: String, files: List[String]): Unit = {
             var (oldhash, newhash) = index.getIndex(file)
             if (oldhash != newhash) {
                 println("error: the following file has changes staged in the index: " + file)
-                sys.exit(1)
             }
-            File(file).delete()
-            index.updateIndex(file, (oldhash, "null"))
+            else {
+                File(file).delete()
+                index.updateIndex(file, (oldhash, "null"))
+            }
+        }
+        else {
+            println(s"error: pathspec '${file}' did not match any files")
         }
     }
 }
